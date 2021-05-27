6600ddc1f7d4f5a350c54ce72447adbdf4f72ee4
abtjade <abt.jade@gmail.com>
2021-05-27 19:01:25 +0900

    Review ESLint, Prettier Settings

---
+++ settings/webpack.config.js
@@ -0,0 +1,18 @@
+/* eslint-disable import/no-unresolved */
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'app.js',
+    path: `${__dirname}/dist`,
+  },
+  devServer: {
+    contentBase: './dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+};
